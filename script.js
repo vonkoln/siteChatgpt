c2a9fa9f347022294407a76e761e69333d72df42
brunovonkoln <brunovonkoln@gmail.com>
2023-02-21 14:11:41 -0300

    ..

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log('Olá, mundo!');
+let time = 3000,
+    currentImageIndex = 0,
+    images = document.querySelectorAll("#slider img")
+    max = images.length
+
+let imagemUsadas = [];
+
+  function nextImage()
+  {
+    images[currentImageIndex].classList.remove("selected")
+
+    // do {
+    //   imagemSelecionada = images[Math.floor(Math.random() * images.length)].classList;
+    // } while (imagemUsadas.includes(imagemSelecionada));
+    // imagemUsadas.push(imagemSelecionada);
+    // currentImageIndex = imagemSelecionada
+
+    currentImageIndex++
+
+    if(currentImageIndex >= max)
+        currentImageIndex = 0
+
+    images[currentImageIndex].classList.add("selected")
+  }
+
+function start()
+{
+  setInterval(() => {
+    nextImage()
+  }, time);
+}
+
+window.addEventListener("load", start)
